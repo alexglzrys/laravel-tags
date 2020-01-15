db04a7ca101bde4f9655f73cb66c565c9225c78f
Alejandro González Reyes <alexglzrys@gmail.com>
2020-01-14 21:02:11 -0600

    list books and labels in the view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,30 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-8 offset-2">
+                <h1 class="mt-4 mb-1"><i class="fas fa-tags"></i> Laravel Book Tags</h1>
+                <p>Sistema de etiquetas basado en el componente laravel-tagging</p>
+                <hr>
+                @foreach($books as $book)
+                    <div class="card  mb-3 border-primary">
+                        <div class="card-header bg-primary text-white">{{ $book->name }}</div>
+                    <div class="card-body">{{ $book->description }}</div>
+                    <div class="card-footer">
+                        @forelse($book->tags as $tag)
+                        <span class="badge badge-info text-white p-1">
+                            {{ $tag->name }}
+                        </span>
+                        @empty
+                            <em>Sin etiquetas</em>
+                        @endforelse
+                    </div>
+                    </div>
+                @endforeach
+                </div>
+            </div>
+        </div>
         <!-- Scripts  -->
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
