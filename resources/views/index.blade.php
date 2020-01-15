b6ae61988c5c454e0053fb169bb857390dad262f
Alejandro González Reyes <alexglzrys@gmail.com>
2020-01-14 21:13:20 -0600

    Add form to register books

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,24 +15,38 @@
         <div class="container">
             <div class="row">
                 <div class="col-8 offset-2">
-                <h1 class="mt-4 mb-1"><i class="fas fa-tags"></i> Laravel Book Tags</h1>
-                <p>Sistema de etiquetas basado en el componente laravel-tagging</p>
-                <hr>
-                @foreach($books as $book)
+                    <h1 class="mt-4 mb-1"><i class="fas fa-tags"></i> Laravel Book Tags</h1>
+                    <p>Sistema de etiquetas basado en el componente laravel-tagging</p>
+                    <hr>
+                    <form action="{{ route('books.store') }}" method="POST">
+                        <div class="form-group">
+                            <label for="name">Nombre del Libro</label>
+                            <input type="text" name="name" id="name" class="form-control" placeholder="Ingrese el nombre del libro">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Descripción</label>
+                            <textarea name="description" id="" cols="30" rows="5" class="form-control" placeholder="Ingrese la descripción detallada del libro"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="tags">Etiquetas</label>
+                            <input type="text" name="tags" id="tags" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </form>
+                    <hr>
+                    @foreach($books as $book)
                     <div class="card  mb-3 border-primary">
                         <div class="card-header bg-primary text-white">{{ $book->name }}</div>
-                    <div class="card-body">{{ $book->description }}</div>
-                    <div class="card-footer">
+                        <div class="card-body">{{ $book->description }}</div>
+                        <div class="card-footer">
                         @forelse($book->tags as $tag)
-                        <span class="badge badge-info text-white p-1">
-                            {{ $tag->name }}
-                        </span>
+                            <span class="badge badge-info text-white p-1">{{ $tag->name }}</span>
                         @empty
                             <em>Sin etiquetas</em>
                         @endforelse
+                        </div>
                     </div>
-                    </div>
-                @endforeach
+                    @endforeach
                 </div>
             </div>
         </div>
